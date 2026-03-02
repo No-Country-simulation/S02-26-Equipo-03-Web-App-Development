ea6741f4e736516945c1f25c22c3184b59a30e5e
scarletvargas <scarletvf47@gmail.com>
2026-03-02 13:25:42 -0500

    refactor(logo): remove shared/components/logo.tsx and update final Logo version

--- shared/components/Logo/Logo.tsx
+++ shared/components/Logo/Logo.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image";
 
-export function GardenAdsLogo({ variant }: { variant?: "white" | "black" }) {
+type GardenAdsLogoProps = {
+  variant?: "white" | "black";
+  width?: number;
+  height?: number;
+};
+
+export function GardenAdsLogo({
+  variant = "black",
+  width = 300,
+  height = 150,
+}: GardenAdsLogoProps) {
   return (
     <Image
       src={variant === "white" ? "/Logo_white.svg" : "/Logo_black.svg"}
       alt="GardenAds Logo"
-      width={300}
-      height={150}
+      width={width}
+      height={height}
       className="mx-auto"
     />
   );
