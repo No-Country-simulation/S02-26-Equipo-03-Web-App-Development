1a3f64f7de11b3b87d5c41585fcbc053730a2642
MauroLaime <maurol.dev@gmail.com>
2026-03-05 03:00:50 -0300

    feat(users-roles): enhance UsersRolesSection with layout and invite button for user management

--- shared/components/Settings/user-roles/UsersRolesSection.tsx
+++ shared/components/Settings/user-roles/UsersRolesSection.tsx
@@ -4,6 +4,8 @@ import { UsersRolesTable } from "./UsersRolesTable";
 import { useSelectedProjectStore } from "@/shared/hooks/use-selected-project-store";
 import { useEffect, useState } from "react";
 import { useRef } from "react";
+import { Button } from "../../ui/button";
+import { Plus } from "lucide-react";
 
 export function UsersRolesSection() {
   const selectedProjectId = useSelectedProjectStore((state) => state.selectedProjectId);
@@ -26,8 +28,25 @@ export function UsersRolesSection() {
   }, [selectedProjectId]);
 
   return (
-    <div className="rounded-xl border border-gray-200 bg-white shadow-sm">
-      <UsersRolesTable usersroles={members} loading={loading} />
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <div>
+          <h2 className="text-xl font-semibold text-[#020617]">Usuarios y Roles</h2>
+          <p className="pt-2 text-sm font-medium text-[#475569]">
+            Gestioná quién tiene acceso a la plataforma. Límite: 5 usuarios.
+          </p>
+        </div>
+        <Button
+          size={"lg"}
+          className="rounded-md border-none bg-[#059669] py-6 text-white hover:bg-[#047857]"
+        >
+          <Plus className="mr-2 h-6 w-6" />
+          Invitar Miembros
+        </Button>
+      </div>
+      <div className="rounded-xl border border-gray-200 bg-white shadow-sm">
+        <UsersRolesTable usersroles={members} loading={loading} />
+      </div>
+    </>
   );
 }
